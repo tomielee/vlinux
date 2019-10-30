f45c21452f89b8205bcce0f2ccec1c61d7497110
jeneljenel <jwwlee@gmail.com>
2019-10-30 11:31:12 +0100

    minor changes in notes

--- kmom10/bthloggen/dev.bash
+++ kmom10/bthloggen/dev.bash
@@ -2,11 +2,17 @@
 # kmom10
  
 
+# Create network
+# sudo docker network create dbwebb
 
-# # BYGGER IMAGE 
+# # BYGGER IMAGES
 
 sudo docker build --rm -t jeneljenel/vlinux-kmom10:server server/
 sudo docker build --rm -t jeneljenel/vlinux-kmom10:client client/
+
+# # Kör containers
+# sudo docker run --rm -d -p 8080:1337 --name myserver --net dbwebb jeneljenel/vlinux-kmom10:server
+# sudo docker run -it --rm  --name myclient --net dbwebb jeneljenel/vlinux-kmom10:client
 
 
 #KILL the server
